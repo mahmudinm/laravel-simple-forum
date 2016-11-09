1360ccb5b046a412d1a4bbd94b5b8ba2083234d9
mmanx <claxmanx@gmail.com>
2016-11-09 09:53:51 +0700

    add database

---
+++ database/migrations/2016_11_09_024306_create_threads_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 50);
+            $table->string('body', 10000);
+            
+            $table->integer('views');
+
+            $table->integer('user_id')->unsigned();
+            $table->integer('category_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('threads');
+    }
+}
