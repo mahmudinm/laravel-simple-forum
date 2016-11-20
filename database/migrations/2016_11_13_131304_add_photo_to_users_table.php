a1f7686bc3e32ab508d67959600e2bb6b3685bb5
mmanx <claxmanx@gmail.com>
2016-11-20 10:53:18 +0700

    change default photo

--- database/migrations/2016_11_13_131304_add_photo_to_users_table.php
+++ database/migrations/2016_11_13_131304_add_photo_to_users_table.php
@@ -14,7 +14,7 @@ class AddPhotoToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('photo')->default('upload/default.png');
+            $table->string('photo')->default('default.png');
         });
     }
 
