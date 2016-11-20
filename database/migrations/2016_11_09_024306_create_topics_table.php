6f4f2d4303191ded6eac4e8563db1fe9ea97f818
mmanx <claxmanx@gmail.com>
2016-11-20 10:13:51 +0700

    change name threads migrations

--- database/migrations/2016_11_09_024306_create_topics_table.php
+++ database/migrations/2016_11_09_024306_create_topics_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateThreadsTable extends Migration
+class CreateTopicsTable extends Migration
 {
     /**
      * Run the migrations.
