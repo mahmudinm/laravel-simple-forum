1242b03b96959ae1f8305254d05ed955f3d2705b
mmanx <claxmanx@gmail.com>
2016-11-20 09:46:02 +0700

    add 404 page not found add readme.md

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('style')
+  <link rel="stylesheet" href="/css/jasny-bootstrap.css">
+@stop
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+          <h1>404 . Page not found </h1>
+        </div>
+    </div>
+</div>
+@endsection
+
+
+
+@section('script')
+  <script src="/js/jasny-bootstrap.js"></script>
+@stop
